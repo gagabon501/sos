15a9542fb95af57213f375a8a697835efaedbc22
Gilberto Gabon <gagabon501@yahoo.com>
2023-06-18 18:08:31 +1200

    Added forced HTTPS on production

--- server.js
+++ server.js
@@ -77,6 +77,17 @@ app.use((req, res, next) => {
   next();
 });
 
+//Forced HTTPS: 18-June-23
+app.use((req, res, next) => {
+  if (!process.env.NODE_ENV == "development") {
+    if (req.headers["x-forwarded-proto"] !== "https")
+      return res.redirect("https://" + req.headers.host + req.url);
+    else return next();
+  } else {
+    return next();
+  }
+});
+
 /**
  * -------------- ROUTES ----------------
  */
